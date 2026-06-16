b312c6de1313db82bd482ba5516390e90869db37
edward-serna <edward.serna@my.utsa.edu>
2026-06-16 13:46:28 -0500

    updated images

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Allow the domain in Next.js
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '*.public.blob.vercel-storage.com' }
-    ]
-  }
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com',
+      },
+    ],
+  },
 };
+
 export default nextConfig;
--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export async function POST(request) {
   const form = await request.formData();
   const file = form.get('file');
-  const blob = await put(file.name, file, { access: 'private' });
+  const blob = await put(file.name, file, { access: 'public' });
 
   return NextResponse.json(blob);
 }
